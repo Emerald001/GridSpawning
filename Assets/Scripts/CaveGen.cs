1327b9af9140401de8b16028a2419995cd3cb23d
Andries <andriesdg199@gmail.com>
2021-09-21 11:48:49 +0200

    Added Project

---
+++ Assets/Scripts/CaveGen.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveGen : MonoBehaviour
+{
+    public int XSize = 10;
+    public int YSize = 10;
+
+    private int[,] map;
+
+    public GameObject SafeCube;
+
+    private void Start()
+    {
+        map = new int[XSize, YSize];
+
+        for (int Xcor = 0; Xcor<XSize; Xcor++)
+        {
+            for (int Ycor = 0; Ycor<YSize; Ycor++)
+            {
+                map[Xcor, Ycor] = 1;
+            }
+        }
+
+        StartHole();
+        CreateHole();
+        SpawnGrid();
+    }
+
+    void SpawnGrid() 
+    {
+        for (int Xcor = 0; Xcor < XSize; Xcor++)
+        {
+            for (int Ycor = 0; Ycor < YSize; Ycor++)
+            {
+                if(map[Xcor, Ycor] == 1)
+                {
+                    Instantiate(SafeCube, new Vector3(Xcor, 0, Ycor), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    void StartHole()
+    {
+        int xPositon = Mathf.RoundToInt(Random.Range(XSize / 3, XSize / 3 * 2));
+        int yPositon = Mathf.RoundToInt(Random.Range(YSize / 3, YSize / 3 * 2));
+
+        map[xPositon, yPositon] = 0;
+    }
+
+    void CreateHole()
+    {
+        for (int Xcor = 0; Xcor < XSize; Xcor++)
+        {
+            for (int Ycor = 0; Ycor < YSize; Ycor++)
+            {
+                if (map[Xcor, Ycor] == 0)
+                {
+                    for (int XNeighbor = -1; XNeighbor <= 1; XNeighbor++)
+                    {
+                        for (int YNeighbor = -1; YNeighbor <= 1; YNeighbor++)
+                        {
+                            if (Xcor + XNeighbor >= 0 && Xcor + XNeighbor < XSize && Ycor + YNeighbor >= 0 && Ycor + YNeighbor < YSize)
+                            {
+                                if (XNeighbor != 0 || YNeighbor != 0)
+                                {
+                                    if(Random.Range(0, 5) >= 3)
+                                    {
+                                        map[Xcor + XNeighbor, Ycor + YNeighbor] = 0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public enum TileStates { 
+        Mine,
+        Flag,
+        Empty,
+    }
+
+    public int XSize = 10;
+    public int YSize = 10;
+    public int chanceOfMines = 5;
+
+    private int[,] map;
+
+    public GameObject GridCube;
+    public GameObject SafeCube;
+    public GameObject Mine;
+
+    private void Start()
+    {
+        map = new int[XSize, YSize];
+
+        for (int i = 0; i < XSize; i++)
+        {
+            for (int I = 0; I < YSize; I++)
+            {
+                map[i, I] = Random.Range(1, chanceOfMines);
+            }
+        }
+        Debug.Log("Created Grid");
+
+        SpawnMines();
+        SpawnBlueBlocks();
+        SpawnGreenblocks();
+    }
+
+    void SpawnMines()
+    {
+        for (int i = 0; i < XSize; i++)
+        {
+            for (int I = 0; I < YSize; I++)
+            {
+                if(map[i, I] == 1)
+                {
+                    Instantiate(Mine, new Vector3(i, 0, I), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    void SpawnNumberBlocks()
+    {
+        for (int i = 0; i < XSize; i++)
+        {
+            for (int I = 0; I < YSize; I++)
+            {
+                if(map[i, I] == 1)
+                {
+                    for (int X = -1; X <= 1; X++)
+                    {
+                        for (int Y = -1; Y <= 1; Y++)
+                        {
+                            if(i + X >= 0 && i + X < XSize && I + Y >= 0 && I + Y < YSize)
+                            {
+                                if (X != 0 || Y != 0)
+                                {
+                                    if (map[i + X, I + Y] != 1)
+                                    {
+                                        Instantiate(SafeCube, new Vector3(i + X, 0, I + Y), Quaternion.identity);
+                                        map[i + X, I + Y] = 0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void SpawnBlueBlocks()
+    {
+
+        for (int i = 0; i < XSize; i++)
+        {
+            for (int I = 0; I < YSize; I++)
+            {
+                if (map[i, I] != 1)
+                {
+                    int minesAround = 0;
+
+                    for (int X = -1; X <= 1; X++)
+                    {
+                        for (int Y = -1; Y <= 1; Y++)
+                        {
+                            if (i + X >= 0 && i + X < XSize && I + Y >= 0 && I + Y < YSize)
+                            {
+                                if (X != 0 || Y != 0)
+                                {
+                                    if(map[i + X, I + Y] == 1)
+                                    {
+                                        minesAround += 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if(minesAround != 0)
+                    {
+                        var BlueCube = Instantiate(SafeCube, new Vector3(i, 0, I), Quaternion.identity);
+                        var renderer = BlueCube.GetComponent<Renderer>();
+                        renderer.material.color = new Color(.4f - .1f * minesAround, .4f - .1f * minesAround, 1);
+                        map[i, I] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    void SpawnGreenblocks()
+    {
+        for (int i = 0; i < XSize; i++)
+        {
+            for (int I = 0; I < YSize; I++)
+            {
+                if(map[i, I] > 1)
+                {
+                    Instantiate(GridCube, new Vector3(i, 0, I), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
